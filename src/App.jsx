2bcde0b2fc8e6c7de6ff390bf60362c410cd6621
Barkha32 <barkha32@gmail.com>
2024-02-27 10:09:58 -0600

    error solving on load

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import Cart from './components/pages/Cart.jsx';
 import AddNewFood from './components/AddNewFood.jsx';
 import UpdateFoodItem from './components/UpdateFoodItem.jsx';
 import Checkout from './components/Checkout.jsx';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { showFood } from './redux/slices/foodSlice.js';
 // import Login from './components/Login.jsx';
 
 
@@ -15,14 +18,24 @@ import Checkout from './components/Checkout.jsx';
 
 
 function App() {
+  const dispatch =useDispatch();
+  useEffect(()=>{
+    dispatch(showFood());
+  
+  },[dispatch]);
+  const foodsdata = useSelector((state) => state.food.foodsdata);
+  const loading = useSelector((state) => state.food.loading);
+  console.log(" state.food", foodsdata);
 
+  
   return (
     <div className='App'> 
       
       <Navbar></Navbar>
+      <Home ></Home>
       <Routes>
       
-        <Route path="/" element={<Home></Home>} />
+         <Route path="/" element={<Home foodsdata={foodsdata} loading={loading}></Home>} /> 
         <Route exact path="/cart" element={<Cart></Cart>} />
         {/* <Route exact path="/login" element={<Login></Login>} /> */}
         <Route exact path="/addnewfood" element={<AddNewFood></AddNewFood>} />
